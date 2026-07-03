0b6f0e262cd6ecee1f7bad85f301235a91e9990e
Varun Singh <varun2.mitmpl2024@learner.manipal.edu>
2026-07-03 22:10:10 +0530

    Add: find-pivot-index - submission-1

---
+++ Data Structures & Algorithms/find-pivot-index/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        n = len(nums)
+        presum=[]
+        postsum=[]
+        pre=0
+        post=0
+        for i in range(n):
+            pre+=nums[i]
+            post+=nums[n-i-1]
+            presum.append(pre)
+            postsum.append(post)
+        postsum.reverse()
+        for j in range(n):
+            if presum[j] == postsum[j]:
+                return j
+        return -1
+    
+    
+
