7545a7f7801813cf0bbfe21e1fac4de62b4cb774
Varun Singh <varun2.mitmpl2024@learner.manipal.edu>
2026-07-03 12:32:03 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        #s=s.lower()
+        #t=t.lower()
+        hasht={}
+        for c in s:
+            if c in hasht:
+                hasht[c] += 1
+            else:
+                hasht[c]=1
+        for c in t:
+            if c not in hasht:
+                return False
+            hasht[c] -= 1
+        for i in hasht:
+            if hasht[i] != 0:
+                return False
+        return True
+        
