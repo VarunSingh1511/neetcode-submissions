cd0daadeb64f5d44d35d7b47f151d40696beddcc
Varun Singh <varun2.mitmpl2024@learner.manipal.edu>
2026-08-11 20:45:02 +0530

    Add: subtree-of-a-binary-tree - submission-3

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-3.py
@@ -0,0 +1,26 @@
+class Solution:   
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def checkSubtree(p, q):
+            if not p and not q:
+                return True
+            if not p or not q:
+                return False
+            return p.val == q.val and checkSubtree(p.left, q.left) and checkSubtree(p.right, q.right)
+
+        def dfs(root, subroot):
+            cur = root
+            stack = []
+            while cur or stack:
+                if cur:
+                    if cur.val == subroot.val:
+                        if checkSubtree(cur, subroot):
+                            return True
+                    stack.append(cur.right)
+                    cur = cur.left
+
+                else:
+                    cur = stack.pop()
+
+            return False
+
+        return dfs(root, subRoot)
